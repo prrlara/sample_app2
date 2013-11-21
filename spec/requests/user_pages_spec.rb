029027adcc7d8cf99cc937257ae355ca6ac807be
prrlara <prrlara@gmail.com>
2013-11-20 22:00:16 -0200

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,10 +31,17 @@ describe "User pages" do
       end
     end
 
+    describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Paulo Rodrigues"
-        fill_in "Email",        with: "prrlara@gmail.com"
+        fill_in "Name",         with: "User Example"
+        fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
@@ -42,6 +49,16 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+    describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Bem vindo') }
     end
+   end
   end
+
 end
